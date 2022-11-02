e7dbf55a2cf54506fbd0e55e99850e6b6fd1e032
sebbjo <94020844+sebbjo@users.noreply.github.com>
2022-11-02 20:42:58 +0100

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -9,7 +9,6 @@ from rest_framework.decorators import api_view
 @api_view(['GET'])
 def get_cars(request):
     cars = Car.objects.all()
-    # customer = Customer.objects.all()
     serializer = CarSerializer(cars, many=True)
     print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -46,7 +45,6 @@ def delete_car(request, id):
 @api_view(['GET'])
 def get_customers(request):
     customers = Customer.objects.all()
-    # customer = Customer.objects.all()
     serializer = CustomerSerializer(customers, many=True)
     print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
@@ -78,4 +76,40 @@ def delete_customer(request, id):
     except Customer.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     theCustomer.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def get_employee(request):
+    employee = Employee.objects.all()
+    serializer = EmployeeSerializer(Employee, many=True)
+    print(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def save_employee(request):
+    serializer = EmployeeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['PUT'])
+def update_employee(request, id):
+    try:
+        theEmployee = Employeer.objects.get(pk=id)
+    except Employee.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = EmployeeSerializer(theEmployee, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def delete_employee(request, id):
+    try:
+        theEmployee = Employee.objects.get(pk=id)
+    except Employee.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    theEmployee.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
