8a0d82eebc0834a9ae8d5ac70da2f91be4834733
KHelgeland <117161346+KHelgeland@users.noreply.github.com>
2022-11-03 15:40:38 +0100

    Update models.py
    La til car_id og customer_id. Endret også integerfields til charfields.

--- mysite/models.py
+++ mysite/models.py
@@ -4,6 +4,7 @@ class Car(models.Model):
     make = models.CharField(max_length=50)
     carmodel = models.CharField(max_length=50)
     year = models.CharField(max_length=4)
+    car_id = models.CharField(max_length=7) # registration number. Ex. RJ 12345
     location = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
 
@@ -12,6 +13,7 @@ class Car(models.Model):
 
     
 class Customer(models.Model):
+    customer_id = models.Charfield(max_length=10)
     name = models.CharField(max_length=50)
     age = models.CharField(max_length=3)
     address = models.CharField(max_length=50)
@@ -31,8 +33,8 @@ class Employee(models.Model):
 
     
 class Order_car(models.Model):
-    car = models.IntegerField()
-    customer = models.IntegerField()
+    car_id = models.CharField(max_length=7) # registration number
+    customer_id = models.CharField(max_length=10)
 
     def __str__(self):
-        return str(self.car) + " " + str(self.customer)
+        return str(self.car_id) + " " + str(self.customer_id)
