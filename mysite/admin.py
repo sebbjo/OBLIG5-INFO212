6c2d323032ac911fccd02a7b64ebc0a9ead8b660
Gareth Mallory <68285584+marius491@users.noreply.github.com>
2022-11-03 14:40:25 +0100

    Update admin.py

--- mysite/admin.py
+++ mysite/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from .models import Car
 from .models import Customer
 from .models import Employee
+from .models import Order_car
 
 admin.site.register(Car)
 admin.site.register(Customer)
 admin.site.register(Employee)
+admin.site.register(Order_car)
