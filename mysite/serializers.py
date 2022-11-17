8ac7dc9b0bff1535ead47ca6d24a84254b1eea91
KHelgeland <117161346+KHelgeland@users.noreply.github.com>
2022-11-17 11:15:31 +0100

    Update serializers.py

--- mysite/serializers.py
+++ mysite/serializers.py
@@ -1,8 +1,5 @@
 from rest_framework import serializers
-from .models import Car
-from .models import Customer
-from .models import Employee
-from .models import Order_car
+from .models import Car, Customer, Employee, Order_car
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,13 +10,14 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'address', 'age']
-        
-class EmployeeSerializer(serializers.ModelSerializer): 
+
+class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
-        fields = ['name', 'age', 'branch']
-        
+        fields = ['id', 'name', 'age', 'branch']
+
 class Order_carSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order_car
         fields = ["id", "car", "customer"]
+        
