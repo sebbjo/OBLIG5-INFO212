bba48cbf6f2807e347d1d91d07b37b26feb0e4d6
KHelgeland <117161346+KHelgeland@users.noreply.github.com>
2022-11-17 11:14:01 +0100

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,126 +1,117 @@
-from .models import Car
-from .models import Customer
-from .models import Employee
-from .models import Order_car
-from .serializers import CarSerializer
-from .serializers import CustomerSerializer
-from .serializers import EmployeeSerializer
-from .serializer import Order_carSerializer
+from .models import Car, Customer, Employee, Order_car
+from .serializers import CarSerializer, EmployeeSerializer, CustomerSerializer, Order_carSerializer
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(["GET"])
 def get_cars(request):
     cars = Car.objects.all()
     serializer = CarSerializer(cars, many=True)
-    print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def save_car(request):
     serializer = CarSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    
-@api_view(['PUT'])
+
+@api_view(["PUT"])
 def update_car(request, id):
     try:
-        theCar = Car.objects.get(pk=id)
+        the_car = Car.objects.get(pk=id)
     except Car.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = CarSerializer(theCar, data=request.data)
+    serializer = CarSerializer(the_car, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    
-@api_view(['DELETE'])
+
+@api_view(["DELETE"])
 def delete_car(request, id):
     try:
-        theCar = Car.objects.get(pk=id)
+        the_car = Car.objects.get(pk=id)
     except Car.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    theCar.delete()
+    the_car.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET'])
-def get_customer(request):
+@api_view(["GET"])
+def get_customers(request):
     customers = Customer.objects.all()
     serializer = CustomerSerializer(customers, many=True)
-    print(serializer.data)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def save_customer(request):
     serializer = CustomerSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    
-@api_view(['PUT'])
+
+
+@api_view(["PUT"])
 def update_customer(request, id):
     try:
-        theCustomer = Customer.objects.get(pk=id)
+        the_customer = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = CustomerSerializer(theCustomer, data=request.data)
+    serializer = CustomerSerializer(the_customer, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    
-@api_view(['DELETE'])
+
+@api_view(["DELETE"])
 def delete_customer(request, id):
     try:
-        theCustomer = Customer.objects.get(pk=id)
+        the_customer = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    theCustomer.delete()
+    the_customer.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view(['GET'])
 def get_employee(request):
-    employee = Employee.objects.all()
-    serializer = EmployeeSerializer(Employee, many=True)
-    print(serializer.data)
+    employees = Employee.objects.all()
+    serializer = EmployeeSerializer(employees, many = True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def save_employee(request):
     serializer = EmployeeSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    
-    
-@api_view(['PUT'])
+
+
+@api_view(["PUT"])
 def update_employee(request, id):
     try:
-        theEmployee = Employee.objects.get(pk=id)
+        the_employee = Employee.objects.get(pk=id)
     except Employee.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    serializer = EmployeeSerializer(theEmployee, data=request.data)
+    serializer = EmployeeSerializer(the_employee, data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data)
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    
+
 @api_view(['DELETE'])
 def delete_employee(request, id):
     try:
@@ -129,13 +120,13 @@ def delete_employee(request, id):
         return Response(status=status.HTTP_404_NOT_FOUND)
     theEmployee.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
-    
-    
+
+
 @api_view(["POST"])
 def order_car(request):
-    #Place order for a car
+    """Place order for a car"""
 
-    order_serializer = Order_carSerializer(data= request.data)
+    order_serializer = Order_carSerializer(data=request.data)
     car_id = request.data['car']
     customer_id = request.data['customer']
 
@@ -157,17 +148,17 @@ def order_car(request):
                 return Response(
                     "Customer has booking registered", status=status.HTTP_403_FORBIDDEN)
         else:
-            return Response("Car already booked", status=status.HTTP_403_FORBIDDEN)
+            return Response("Car is already booked", status=status.HTTP_403_FORBIDDEN)
     return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(["DELETE"])
 def cancel_ordered_car(request):
-    #Cancel an existing order
+    """Cancel an existing order"""
 
     car_id = request.data['car']
     customer_id = request.data['customer']
-    cancel_order = Order_car.objects.get(car = car_id, customer = customer_id)
+    cancel_order = Order_car.objects.get(car=car_id, customer=customer_id)
     try:
         if cancel_order.car == car_id and cancel_order.customer == customer_id:
             Car.objects.filter(pk=car_id).update(status='available')
@@ -176,11 +167,11 @@ def cancel_ordered_car(request):
     except Order_car.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-
+    
 @api_view(["PUT"])
 def rent_car(request):
-    #Change status from booked to rented
-    
+    """Change car status to rented"""
+
     car_id = request.data['car']
     customer_id = request.data['customer']
     the_order = Order_car.objects.get(car = car_id, customer = customer_id)
@@ -192,7 +183,7 @@ def rent_car(request):
 
 @api_view(["DELETE"])
 def return_car(request):
-    #return car to dealer
+    """Return the car to dealer"""
 
     car_id = request.data['car']
     customer_id = request.data['customer']
@@ -203,6 +194,7 @@ def return_car(request):
             the_status = request.data['status']
     except:
         pass
+
 
     if the_order.car == car_id and the_order.customer == customer_id:
         Car.objects.filter(pk=car_id).update(status=the_status)
