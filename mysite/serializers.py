aff6d33601fbd0506c1185ba36b0352eaa2a10cb
sebbjo <94020844+sebbjo@users.noreply.github.com>
2022-11-19 13:56:43 +0100

    Update serializers.py

--- mysite/serializers.py
+++ mysite/serializers.py
@@ -6,16 +6,19 @@ class CarSerializer(serializers.ModelSerializer):
         model = Car
         fields = ['id', 'make', 'carmodel', 'year', 'location', 'status']
 
+        
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = ['id', 'name', 'address', 'age']
 
+        
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Employee
         fields = ['id', 'name', 'age', 'branch']
 
+        
 class Order_carSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order_car
